d2e108ad27c9ce78eb75d5c64bffd8ecf78acdbd
zunda <zunda.dev@gmail.com>
2023-07-06 02:21:17 +0900

    replace to UserProfileImage

--- GitBoardKit/Sources/GitBoardUI/UserCell/UserCell.swift
+++ GitBoardKit/Sources/GitBoardUI/UserCell/UserCell.swift
@@ -11,23 +11,13 @@ struct UserCell: View {
   
   var body: some View {
     HStack(alignment: .top, spacing: 10) {
-      let circleLineWidth: CGFloat = 3
       let profileImageSize: CGFloat = 60
       
-      CachedAsyncImage(url: user.avatarURL) { image in
-        image.resizable()
-      } placeholder: {
-        Image(systemName: "person")
-          .resizable()
-          .padding(10)
-      }
-      .frame(width: profileImageSize, height: profileImageSize)
-      .clipShape(.circle)
-      .padding(circleLineWidth / 2)
-      .overlay {
-        Circle()
-          .stroke(.thinMaterial, lineWidth: circleLineWidth)
-      }
+      UserProfileImage(
+        avatarURL: user.avatarURL,
+        type: user.type
+      )
+        .frame(width: profileImageSize, height: profileImageSize)
 
       VStack(alignment: .leading, spacing: 4) {
         HStack(alignment: .center, spacing: 5) {
@@ -48,12 +38,10 @@ struct UserCell: View {
         HStack(alignment: .center, spacing: 10) {
           if let company = user.company {
             Text("\(Image(systemName: "building")) \(company)")
-              .font(.caption)
           }
           
           if let location = user.location {
             Text("\(Image(systemName: "location")) \(location)")
-              .font(.caption)
           }
         }
       }
@@ -63,52 +51,7 @@ struct UserCell: View {
 
 struct UserCell_Preview: PreviewProvider {
   static var previews: some View {
-    let user = User(
-      id: 1234,
-      userID: "zunda-pixel",
-      userName: "zunda",
-      nodeID: "",
-      avatarURL: URL(string: "https://avatars.githubusercontent.com/u/47569369?v=4")!,
-      gravatarID: "",
-      url: URL(string: "https://github.com")!,
-      htmlURL: URL(string: "https://github.com")!,
-      followersURL: URL(string: "https://github.com")!,
-      followingURL: URL(string: "https://github.com")!,
-      subscriptionsURL: URL(string: "https://github.com")!,
-      organizationsURL: URL(string: "https://github.com")!,
-      reposURL: URL(string: "https://github.com")!,
-      eventsURL: URL(string: "https://github.com")!,
-      receivedEventsURL: URL(string: "https://github.com")!,
-      gistsURL: URL(string: "https://github.com")!,
-      starredURL: URL(string: "https://github.com")!,
-      publicRepoCount: 1,
-      totalPrivateRepoCount: 2,
-      ownedPrivateRepoCount: 3,
-      publicGistsCount: 2,
-      privateGistsCount: 3,
-      followerCount: 3,
-      followingCount: 4,
-      createdAt: .now,
-      updatedAt: .now,
-      bio: "bio",
-      email: "email",
-      location: "Japan",
-      hireable: true,
-      type: .user,
-      score: 1,
-      siteAdmin: true,
-      twitterUserName: "zunda_pixel",
-      company: "Apple",
-      diskUsage: 12312,
-      collaboratorCount: 0,
-      twoFactorAuthentication: false,
-      plan: .init(
-        name: "free",
-        space: 32,
-        collaboratorCount: 342,
-        privateRepoCount: 4234
-      )
-    )
+    let user = User.sample
     
     List {
       UserCell(user: user)
@@ -117,4 +60,53 @@ struct UserCell_Preview: PreviewProvider {
     }
     .listStyle(.plain)
   }
+}
+
+extension User {
+  static let sample = User(
+    id: 1234,
+    userID: "zunda-pixel",
+    userName: "zunda",
+    nodeID: "",
+    avatarURL: URL(string: "https://avatars.githubusercontent.com/u/47569369?v=4")!,
+    gravatarID: "",
+    url: URL(string: "https://github.com")!,
+    htmlURL: URL(string: "https://github.com")!,
+    followersURL: URL(string: "https://github.com")!,
+    followingURL: URL(string: "https://github.com")!,
+    subscriptionsURL: URL(string: "https://github.com")!,
+    organizationsURL: URL(string: "https://github.com")!,
+    reposURL: URL(string: "https://github.com")!,
+    eventsURL: URL(string: "https://github.com")!,
+    receivedEventsURL: URL(string: "https://github.com")!,
+    gistsURL: URL(string: "https://github.com")!,
+    starredURL: URL(string: "https://github.com")!,
+    publicRepoCount: 1,
+    totalPrivateRepoCount: 2,
+    ownedPrivateRepoCount: 3,
+    publicGistsCount: 2,
+    privateGistsCount: 3,
+    followerCount: 3,
+    followingCount: 4,
+    createdAt: .now,
+    updatedAt: .now,
+    bio: "bio",
+    email: "email",
+    location: "Japan",
+    hireable: true,
+    type: .user,
+    score: 1,
+    siteAdmin: true,
+    twitterUserName: "zunda_pixel",
+    company: "Apple",
+    diskUsage: 12312,
+    collaboratorCount: 0,
+    twoFactorAuthentication: false,
+    plan: .init(
+      name: "free",
+      space: 32,
+      collaboratorCount: 342,
+      privateRepoCount: 4234
+    )
+    )
 }
