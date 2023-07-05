923d9baed935e46912c0dc062838dd9526c3939a
zunda <zunda.dev@gmail.com>
2023-07-06 02:42:08 +0900

    add SearchView

---
+++ GitBoardKit/Sources/GitBoardUI/Search/SearchView.swift
@@ -0,0 +1,42 @@
+//
+//  SearchView.swift
+//
+
+import SwiftUI
+import ToastView
+
+struct SearchView: View {
+  @Bindable var router = NavigationRouter()
+  @State var errorHandle = ErrorHandle()
+  
+  var body: some View {
+    NavigationStack(path: $router.items) {
+      List {
+        NavigationLink(item: .searchUsers) {
+          Label("Search Users", systemImage: "figure.pilates")
+        }
+        
+        NavigationLink(item: .searchRepositories) {
+          Label("Search Repositories", systemImage: "figure.pilates")
+        }
+      }
+      .listStyle(.insetGrouped)
+      .navigationDestination()
+      .navigationTitle("Search")
+    }
+    .toastAlert(
+      item: $errorHandle.error,
+      position: .top,
+      animation: .spring,
+      duration: .seconds(2)
+    ) { error in
+      ErrorView(error: error.error)
+    }
+    .environment(router)
+    .environment(errorHandle)
+  }
+}
+
+#Preview {
+  SearchView()
+}
