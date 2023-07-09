608c0945f144ddfd13ef1e57ecc266660e5af271
zunda <zunda.dev@gmail.com>
2023-07-09 17:48:41 +0900

    add teamID

--- GitBoardKit/Sources/GitBoardData/Env/Env.swift
+++ GitBoardKit/Sources/GitBoardData/Env/Env.swift
@@ -2,8 +2,11 @@
 //  Env.swift
 //
 
+import Foundation
+
 public enum Env {
   static public let clientID: String = <#CLIENT_ID#>
   static public let clientSecret: String = <#CLIENT_SECRET#>
   static public let appGroup = "group.zunda.gitboard"
+  static let teamID: String = Bundle.main.infoDictionary!["AppIdentifierPrefix"] as! String
 }
