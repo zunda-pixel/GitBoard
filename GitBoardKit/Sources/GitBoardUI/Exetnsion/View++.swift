1a1a22fbb4aa4f4c823e226baef2c7bde6a33f67
zunda <zunda.dev@gmail.com>
2023-07-15 02:54:46 +0900

    add listRowSeparator

--- GitBoardKit/Sources/GitBoardUI/Exetnsion/View++.swift
+++ GitBoardKit/Sources/GitBoardUI/Exetnsion/View++.swift
@@ -7,11 +7,11 @@ import SwiftUI
 extension View {
   func listRow() -> some View {
     self
-      .frame(maxWidth: .infinity, alignment: .leading)
       .listRowInsets(EdgeInsets())
       .alignmentGuide(.listRowSeparatorLeading) { _ in
           return 0
       }
       .contentShape(.rect)
+      .listRowSeparator(.hidden)
   }
 }
