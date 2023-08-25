c114f0f51db08a2c8559ce5bf7a40bb4154c0a46
zunda <zunda.dev@gmail.com>
2023-08-25 16:31:16 +0900

    merge appGroup

--- GitBoardKit/Sources/GitBoardData/Env/Env.swift
+++ GitBoardKit/Sources/GitBoardData/Env/Env.swift
@@ -7,7 +7,6 @@ import Foundation
 public enum Env {
   static public let clientID: String = <#CLIENT_ID#>
   static public let clientSecret: String = <#CLIENT_SECRET#>
-  static public let appGroupPrefix = "group"
-  static public let appGroup = "zunda.gitboard"
   static let teamID: String = Bundle.main.infoDictionary!["AppIdentifierPrefix"] as! String
+  static public let appGroup = "group.zunda.gitboard"
 }
