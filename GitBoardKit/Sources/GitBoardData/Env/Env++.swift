d9ffb83e4ad65ac8a1e9aeb628570e49a5e01cb3
zunda <zunda.dev@gmail.com>
2023-08-27 05:28:52 +0900

    add Env++

---
+++ GitBoardKit/Sources/GitBoardData/Env/Env++.swift
@@ -0,0 +1,16 @@
+//
+//  Env++.swift
+//
+
+import Foundation
+
+extension Env {
+  static public let appGroup = "group.zunda.gitboard"
+  static var appIdentifierPrefix: String {
+    var teamID = Bundle.main.infoDictionary!["AppIdentifierPrefix"] as! String
+    if teamID.last == "." {
+      teamID.removeLast() // remove [.] dot
+    }
+    return teamID
+  }
+}
