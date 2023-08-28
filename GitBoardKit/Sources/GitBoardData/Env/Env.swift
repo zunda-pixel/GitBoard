12dc19268e2ec669fde65d971038b3aec37aa6ef
zunda <zunda.dev@gmail.com>
2023-08-28 18:34:36 +0900

    remove appGroup, appIdentifierPrefix

--- GitBoardKit/Sources/GitBoardData/Env/Env.swift
+++ GitBoardKit/Sources/GitBoardData/Env/Env.swift
@@ -7,12 +7,4 @@ import Foundation
 public enum Env {
   static public let clientID: String = <#CLIENT_ID#>
   static public let clientSecret: String = <#CLIENT_SECRET#>
-  static public let appGroup = "group.zunda.gitboard"
-  static var appIdentifierPrefix: String {
-    var teamID = Bundle.main.infoDictionary!["AppIdentifierPrefix"] as! String
-    if teamID.last == "." {
-      teamID.removeLast() // remove [.] dot
-    }
-    return teamID
-  }
 }
