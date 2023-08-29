53e5db64b23ad92533b80ab4a9086ce8ecc992a9
zunda <zunda.dev@gmail.com>
2023-08-30 00:38:37 +0900

    add SecureEnv

---
+++ GitBoardKit/Sources/GitBoardData/Env/SecureEnv.swift
@@ -0,0 +1,10 @@
+//
+//  SecureEnv.swift
+//
+
+import Foundation
+
+public enum SecureEnv {
+  static public let clientID: String = <#CLIENT_ID#>
+  static public let clientSecret: String = <#CLIENT_SECRET#>
+}
