7f985bda5f91c2392b276073da7452cecd3b58a7
zunda <zunda.dev@gmail.com>
2023-09-19 20:37:10 +0900

    fix Image

--- GitBoardKit/Sources/GitBoardUI/User/UserProfileImage.swift
+++ GitBoardKit/Sources/GitBoardUI/User/UserProfileImage.swift
@@ -23,12 +23,32 @@ struct UserProfileImage: View {
 
   var body: some View {
     LazyImage(url: avatarURL) { state in
-      if let image = state.image {
-        image
+      switch state.result {
+      case .success(let result):
+        #if os(macOS)
+        Image(nsImage: result.image)
           .resizable()
-      } else {
+        #else
+        Image(uiImage: result.image)
+          .resizable()
+        #endif
+      case .failure(_):
         Image(systemName: "person")
           .resizable()
+          .overlay {
+            Image(systemName: "xmark")
+              .resizable()
+              .foregroundStyle(.red)
+          }
+      case .none:
+        ProgressView(
+          value: state.progress.fraction,
+          total: Float(state.progress.total)
+        ) {
+          Image(systemName: "person")
+            .resizable()
+            .frame(maxWidth: .infinity)
+        }
       }
     }
     .overlay {
@@ -42,4 +62,12 @@ struct UserProfileImage: View {
         type: type, cornerSize: .init(width: roundWidth * 2, height: roundWidth * 2))
     )
   }
+}
+
+#Preview {
+  UserProfileImage(
+    avatarURL: URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png12")!,
+    type: .user
+  )
+  .frame(width: 100, height: 100)
 }
